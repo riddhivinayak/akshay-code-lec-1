6c9be3954124da2c775b6de647954852a31e32c7
riddhi vinayak <riddhivinayak001@gmail.com>
2024-12-28 16:11:43 +0530

    playing with diffrent htttp methods

--- app.js
+++ app.js
@@ -29,6 +29,24 @@ app.use("/bye",(req,res)=>
                 });
                     
                 
+app.get("/users",(req,res)=>
+    {
+        res.send(  {firstname:"riddhi",lastname:"vinayak"});
+    });
+    
+    
+app.post("/users",(req,res)=>
+    {
+        console.log("save your info in the data base ");
+        res.send("posted in the data bse")
+    });
+
+    
+app.delete("/hello",(req,res)=>
+    {
+        res.send("deleted data from the data base");
+    });
+    
 app.listen(3000,(req,res)=>
     {
     console.log("hi from server");
